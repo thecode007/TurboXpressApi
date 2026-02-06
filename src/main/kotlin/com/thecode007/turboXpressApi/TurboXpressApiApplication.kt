ef3942e06ce454892f63070f7e6834b7fb5631d1
Abdallah Safieddine <30600335+thecode007@users.noreply.github.com>
2026-02-06 16:55:52 +0200

    First commit

---
+++ src/main/kotlin/com/thecode007/turboXpressApi/TurboXpressApiApplication.kt
@@ -0,0 +1,11 @@
+package com.thecode007.turboXpressApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TurboXpressApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TurboXpressApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/thecode007/turboXpressApi/TurboXpressApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.thecode007.turboXpressApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TurboXpressApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
